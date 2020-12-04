62969779602b55bbdf7f9437530ee0a5c3393093
OKUGAWA Yuichi <yokugawa@ccw03.cc.kek.jp>
2020-12-04 14:03:29 +0900

    rm

--- myProcessors/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cs004/yuichi/working/MonoPhoton/myProcessors
-# Build directory: /home/cs004/yuichi/working/MonoPhoton/myProcessors/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
